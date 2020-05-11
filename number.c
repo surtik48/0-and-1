4fed18e0197b6906b40b13f65c35ba59d7096e49
surtik48 <65162663+surtik48@users.noreply.github.com>
2020-05-11 16:18:17 +0530

    number.c
    binary number checker by surtik48

---
+++ number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{int num;  //variable to store number
+printf("enter the number");
+scanf("%d",&num);
+switch(num)  //switch case to check
+{
+case 0:
+printf("i understand");
+break;
+case 1:
+printf("i understand");
+break;
+default:printf("I am not too with numbers other than 0 and 1");
+}
+
+}
